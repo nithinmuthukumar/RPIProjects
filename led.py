12f687b77405dfacc84cc425886786b64cef0e9b
Nithin Muthukumar <kathrikai30201094@gmail.com>
2019-12-03 19:39:21 -0500

    new tut

--- led.py
+++ led.py
@@ -2,20 +2,14 @@ import RPi.GPIO as GPIO
 import time
 import atexit
 
-#PIN setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.OUT)
-
-# Loop to send HIGH and LOW to the mentioned pin
-while True:
-    print("High")
-    GPIO.output(8,GPIO.HIGH)
-    time.sleep(1)
-    print("Low")
-    GPIO.output(8,GPIO.LOW)
-    time.sleep(1)
-
-#Garabage Cleaner
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+print("LED on")
+GPIO.output(18,GPIO.HIGH)
+time.sleep(1)
+print("LED off")
+GPIO.output(18,GPIO.LOW)
 def clean_up():
     GPIO.cleanup()
 
