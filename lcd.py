665ee5ff141b869c628022df362a452244001d41
Nithin Muthukumar <kathrikai30201094@gmail.com>
2019-12-05 19:24:41 -0500

    added lcd file

--- lcd.py
+++ lcd.py
@@ -1,3 +1,5 @@
+from RPi import GPIO
 from RPLCD import CharLCD
+GPIO.setmode(GPIO.BCM)
 lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[40, 38, 36, 32, 33, 31, 29, 23])
 lcd.write_string(u'Hello world!')
